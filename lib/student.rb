bd4aa5946186d4c59a4fd991a80fb6cec66dd1a1
William J Teskey <willjteskey@gmail.com>
2018-07-09 19:09:08 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -7,7 +7,7 @@ def initialize(name, grade)
   @name, @grade = name,grade
 end #initialize
 
-def self.
+def self.create_table
   
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]  
